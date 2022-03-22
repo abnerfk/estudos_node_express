3105749d1116e6a8a0ddbef78e63f8878c80c5e6
Abner Klug <abnerfklug@gmail.com>
2022-03-22 20:30:19 -0300

    teste

---
+++ routes/userRoute.js
@@ -0,0 +1,40 @@
+const { response } = require('express')
+const fs = require('fs')
+const { join } = require('path')
+
+const filePath = join(__dirname, 'users.json')
+
+const getUsers = () => {
+    const data = fs.existsSync(filePath)
+        ? fs.readFileSync(filePath)
+        : []
+    try{
+        return JSON.parse(data)
+    } catch (error) {
+        return []
+    }
+        
+}
+
+const saveUser = (users) => fs.writeFileSync(filePath, JSON.stringify(users, null, '\%'))
+
+const userRoute = (app) => {
+    app.route('/users/:id?')
+        .get((req, res) => {
+            const users = getUsers()
+
+            res.send({ users })
+
+        })
+        .post((req, res)=> {
+            const users = getUsers()
+            users.push(req.body)
+            saveUser(users)
+
+            res.send(201).send('OK')
+
+
+        })
+}
+
+module.exports = userRoute
